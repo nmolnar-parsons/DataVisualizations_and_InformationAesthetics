8c06a01bebbbd54f253a250a89be79e1f9d74fd8
nmolnar-parsons <molnn656@newschool.edu>
2025-09-16 09:52:16 -0400

    Adding exercise 1

---
+++ Exercise1/Exercise1 Sketches/Sketches_Feedback.md
@@ -0,0 +1,15 @@
+## Temporal Data Sketches discussion/crit
+
+- can pre-draw svgs in illustrator and reveal over time with opacity 
+    - for complicated shapes/designs
+    - pre-defined/canned SVG vs drawing from scratch
+
+- my stuff:
+    - most votes for ring/tree diagram and calendar/box diagram
+    - bird migration, 
+
+
+- For prototype:
+    - need all the ingredients (data, psuedocode, etc) for making the viz 
+    - basic coding
+
---
+++ Exercise1/Exercise1_Psuedocode.js
@@ -0,0 +1,16 @@
+// Tree Ring Chart
+
+    // create dummy data (or look for wine harvest data by tonnage)
+
+    // generate circles with radius = data + previous data points
+        // generate largest circle last and generate smaller circles on top with alternating colors
+
+
+
+// Box calendar?
+
+    //  create a grid of squares (12 * 31)
+        // for months with less than 31 days, leave squares blank or gray?
+            // which would be accomplished by data with null values? 
+    // fill in each square based on data (color)
+    // add month and day labels
