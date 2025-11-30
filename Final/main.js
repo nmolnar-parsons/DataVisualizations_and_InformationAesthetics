c9baf010567992cdb0462867c0783d406e17e5e3
nmolnar-parsons <molnn656@newschool.edu>
2025-11-30 18:01:02 -0500

    intro figures and scrolly changes

--- Final/main.js
+++ Final/main.js
@@ -1,29 +1,14 @@
-// Two plots: 
-    // Scatterplot on the left taking up 1/2 window.width
-        //Scatterplot from exercise 2
-        // runtime and viewcount 
-    // Barplot on right taking up 1/2 window.width
-        //text frequency from title
-        //or title length (number of words, or number of characters?)
+// todo:
+    // calculate "the average netflix movie"
+        // average runtimeMinutes and average Views
+    // calculate for each genre and for every runtimeMinutes between 80 and 130 minutes 
 
-
-// Movie Data
-
-
-//Legend:
-    // Checkboxes filter out pre-selected genres
-    // this needs a state variable to hold which genres are checked
-
-// Slider:
-    // filter movies by viewcount (vertically)
-
-
-
-
-// THIS WILL BECOME A SCROLLYTELLING STORY????
-// one scatterplot which we follow
-    // each section will filter to a different genre as that genre's story is told
-    //https://russellsamora.github.io/scrollama/basic/
+    //figure out genre-specific stories
+    
+    // Intro Scatterplot:
+        // looks like it's shifted to the left too much
+        // highlight top 10 movies?
+        // box or border area for "irregulars?"
 
 
 
@@ -38,6 +23,9 @@
 
 let dimensions = [window.innerWidth/2.8, window.innerHeight*0.5] // Make plots even smaller to fit properly
 let margins = {top: 10, right: 20, bottom: 30, left: 40}; // Reduce margins
+
+let intro_dimensions = [window.innerWidth*0.9, window.innerHeight*0.5];
+let intro_margins = {top: 10, right: 200, bottom: 30, left: 250};
 
 // Genres:
 let genres = ["Action", "Comedy", "Drama", "Romance", "Adventure", "Animation", "Documentary", "Horror"];
@@ -110,6 +98,7 @@ async function load_data(){
     barplot_y_max = d3.max(allBarData, d => d.count);
 
     // drawing can only happen after data is here
+    draw_intro();
     draw();
 
 }
@@ -210,6 +199,55 @@ function initializeLayout(){
         .style("font-size", "14px")
         .style("z-index", "1000")
         .style("display", "none");
+
+    //dimensions for top10 figure
+    const svgWidth_top10 = intro_dimensions[0];
+    const svgHeight_top10 = intro_dimensions[1];
+
+
+    //Top 10 List barchart in <div id="top-10-figure">
+    top_10_figure = d3.select("#top-10-figure")
+        .append("svg")
+        .attr("width", svgWidth_top10)
+        .attr("height", svgHeight_top10);
+        // axes for top10:
+    //vertical position is 1-10 using scaleBand
+    top10_y_scale = d3.scaleBand()
+        .range([intro_margins.top, svgHeight_top10 - intro_margins.bottom])
+        .padding(0.1);
+    // horizontal position is runtimeMinutes, linear scale
+    top10_x_scale = d3.scaleLinear()
+        .range([intro_margins.left, svgWidth_top10 - intro_margins.right]);
+
+    top_10_figure.append("g")
+        .attr("class", "top10_x-axis")
+        .attr("transform", `translate(0,${svgHeight_top10 - intro_margins.bottom})`);
+
+    top_10_figure.append("g")
+        .attr("class", "top10_y-axis")
+        .attr("transform", `translate(${intro_margins.left},0)`);
+
+    // Into figure uses the structure of scatterplot but with margins and dimensions from intro
+    intro_scatterplot = d3.select("#intro-scatterplot")
+        .append("svg")
+        .attr("width", svgWidth_top10)
+        .attr("height", svgHeight_top10)
+        .attr("id", "intro_scatterplot_svg");
+    
+    //set up ranges for intro scatterplot scales
+    intro_x_scale = d3.scaleLinear().range([intro_margins.left, svgWidth_top10 - intro_margins.right]);
+    intro_y_scale = d3.scaleLinear().range([svgHeight_top10 - intro_margins.bottom, intro_margins.top]);
+
+    //axes
+    intro_scatterplot.append("g")
+        .attr("class", "intro_scatter_x-axis")
+        .attr("transform", `translate(0,${svgHeight_top10 - intro_margins.bottom})`);
+    intro_scatterplot.append("g")
+        .attr("class", "intro_scatter_y-axis")
+        .attr("transform", `translate(${intro_margins.left},0)`);
+
+
+
 }
 
 function onCheckboxChange(genre){
@@ -242,9 +280,6 @@ function onCheckboxChange(genre){
 
 
 
-
-
-
 function draw(){
     // Helper to get the genre used for color and filtering (always agrees)
     function getColorGenre(d) {
@@ -261,8 +296,11 @@ function draw(){
     // Scatterplot:
 
     // domains for scales
-    x_scale.domain(d3.extent(data, d => d.fixed_minutes)).nice();
-    y_scale.domain(d3.extent(data, d => d.Views)).nice();
+    // x_scale.domain(d3.extent(data, d => d.fixed_minutes)).nice();
+    // y_scale.domain(d3.extent(data, d => d.Views)).nice();
+    //domains for scales are 80 to 130 minutes and 0 to 80M Views in filtered data
+    x_scale.domain([75, 150]).nice();
+    y_scale.domain([0, 80000000]).nice();
     //color has been set already
 
     // select svg
@@ -354,17 +392,8 @@ function draw(){
 
     scatterplot_svg.select(".scatter_y-axis")
         .call(d3.axisLeft(y_scale).ticks(6).tickFormat(d3.format(".2s")));
-   
-
-
-
-
-
-
-
-
         
-            // Barplot:
+    // Barplot:
     
     // Get word frequencies from titles
     let words = [];
@@ -435,8 +464,7 @@ function draw(){
     bars.exit().remove();
 
     // UPDATE existing bars
-    bars
-        .transition()
+    bars.transition()
         .duration(400)
         .attr("x", d => x_scale_bar(d.word))
         .attr("y", d => y_scale_bar(d.count))
@@ -522,8 +550,171 @@ function draw(){
         .transition()
         .duration(600)
         .style("opacity", 1);
-   
 }
+
+
+function draw_intro(){
+    // get 10 movies with highest Views
+    let top10 = Array.from(data);
+    top10.sort((a,b) => d3.descending(a.Views, b.Views));
+    top10 = top10.slice(0,10);
+
+    // set domains for scales
+    top10_y_scale.domain(top10.map((d,i) => i+1)); // 1-10
+    top10_x_scale.domain([0, d3.max(top10, d => d.fixed_minutes)]).nice();
+
+    // draw bars
+    const bars = top_10_figure.selectAll(".top10-bar")
+        .data(top10, d => d.primaryTitle);
+
+    bars.enter()
+        .append("rect")
+        .attr("class", "top10-bar")
+        .attr("x", intro_margins.left)
+        .attr("y", (d,i) => top10_y_scale(i+1))
+        .attr("width", d => top10_x_scale(d.fixed_minutes) - margins.left)
+        .attr("height", top10_y_scale.bandwidth())
+        .attr("fill", "#e50914");
+
+    // update axes
+    top_10_figure.select(".top10_x-axis")
+        .call(d3.axisBottom(top10_x_scale).ticks(6).tickFormat(d => d + " min"));
+
+    top_10_figure.select(".top10_y-axis")
+        .call(d3.axisLeft(top10_y_scale).tickFormat((d,i) => `${d}. ${top10[i].primaryTitle}`));
+    
+    //Text labels for left side of bars
+    const labels = top_10_figure.selectAll(".top10-bar-label")
+        .data(top10, d => d.primaryTitle);
+
+    labels.enter()
+        .append("text")
+        .attr("class", "top10-bar-label")
+        .attr("text-anchor", "end")
+        .attr("fill", "white")
+        .attr("font-size", "12px")
+        .attr("x", intro_margins.left - 10)
+        .attr("y", (d,i) => top10_y_scale(i+1) + top10_y_scale.bandwidth()/2 + 4)
+        .text(d => d.primaryTitle);
+
+    //add runtime labels at end of bars
+    const runtimeLabels = top_10_figure.selectAll(".top10-bar-runtime-label")
+        .data(top10, d => d.primaryTitle);
+
+    runtimeLabels.enter()
+        .append("text")
+        .attr("class", "top10-bar-runtime-label")
+        .attr("text-anchor", "start")
+        .attr("fill", "white")
+        .attr("font-size", "12px")
+        .attr("x", d => top10_x_scale(d.fixed_minutes)+150)
+        .attr("y", (d,i) => top10_y_scale(i+1) + top10_y_scale.bandwidth()/2 + 4)
+        .text(d => `${d.fixed_minutes} min`);
+    
+    //console.log combined viewcount
+    let totalViews = d3.sum(top10, d => d.Views);
+    console.log("Total Views for Top 10 Movies:", totalViews);
+    
+    // add views for each film at base of each bar. class("top10-viewcount-label")
+    const viewcountLabels = top_10_figure.selectAll(".top10-viewcount-label")
+        .data(top10, d => d.primaryTitle);
+
+    viewcountLabels.enter()
+        .append("text")
+        .attr("class", "top10-viewcount-label")
+        .attr("text-anchor", "start")
+        .attr("fill", "black")
+        .attr("font-size", "10px")
+        .attr("x", intro_margins.left + 5 )
+        .attr("y", (d,i) => top10_y_scale(i+1) + top10_y_scale.bandwidth()/2 + 4)
+        .text(d => `${d3.format(",")(d.Views)} views`);
+
+    // Intro scatterplot:
+    // domains for intro scatterplot scales
+    intro_x_scale.domain(d3.extent(data, d => d.fixed_minutes)).nice();
+    intro_y_scale.domain(d3.extent(data, d => d.Views)).nice();
+
+    // select svg
+    const intro_scatterplot_svg = d3.select("#intro_scatterplot_svg");
+
+    // draw points
+    intro_scatterplot_svg.selectAll(".intro-point")
+        .data(data)
+        .enter()
+        .append("circle")
+        .attr("class", "intro-point")
+        .attr("cx", d => intro_x_scale(d.fixed_minutes))
+        .attr("cy", d => intro_y_scale(d.Views))
+        .attr("r", 4)
+        .attr("fill", d => {
+            if (d.genres.includes("Animation")) return color_scale("Animation");
+            if (d.genres.includes("Romance")) return color_scale("Romance");
+            return color_scale(d.genres[0]);
+        }) // add tooltip
+        .on("mouseover", function(event, d) {
+            const tooltip = d3.select("#tooltip");
+            tooltip
+                .style("display", "block")
+                .html(
+                    `<strong>${d.primaryTitle}</strong><br/>
+                    ${d.fixed_minutes} min<br/>
+                    ${d.genres.join(", ")}<br/>
+                    ${d3.format(",")(d.Views)} views`
+                );
+            d3.select(this)
+                .attr("stroke", "#fff")
+                .attr("stroke-width", 2)
+                .attr("r", 6); // increase r to 6 on hover
+        })
+        .on("mousemove", function(event) {
+            const tooltip = d3.select("#tooltip");
+            tooltip
+                .style("left", (event.pageX + 16) + "px")
+                .style("top", (event.pageY - 24) + "px");
+        })
+        .on("mouseleave", function() {
+            const tooltip = d3.select("#tooltip");
+            tooltip.style("display", "none");
+            d3.select(this)
+                .attr("stroke", null)
+                .attr("stroke-width", null)
+                .attr("r", 4); // decrease r back to 4 on mouseleave
+        });
+    //highlight top 10 movies in intro scatterplot
+    intro_scatterplot_svg.selectAll(".intro-point")
+        .attr("stroke", d => top10.includes(d) ? "#ffb700ff" : null)
+        .attr("stroke-width", d => top10.includes(d) ? 2 : null)
+        .attr("r", d => top10.includes(d) ? 8 : 4)
+        //mouse leave should not remove highlight or radius for top10
+        .on("mouseleave", function(event, d) {
+            const tooltip = d3.select("#tooltip");
+            tooltip.style("display", "none");
+            d3.select(this)
+                .attr("stroke", d => top10.includes(d) ? "#ffb700ff" : null)
+                .attr("stroke-width", d => top10.includes(d) ? 2 : null)
+                .attr("r", d => top10.includes(d) ? 8 : 4); // keep radius for top10
+        });
+        
+
+    // Update axes
+    intro_scatterplot_svg.select(".intro_scatter_x-axis")
+        .call(d3.axisBottom(intro_x_scale).ticks(6).tickFormat(d => d + " min"))
+        .selectAll("path, line") // Select axis lines and ticks
+        .attr("stroke", "white"); // Set stroke to white
+
+    intro_scatterplot_svg.select(".intro_scatter_y-axis")
+        .call(d3.axisLeft(intro_y_scale).ticks(6).tickFormat(d3.format(".2s")))
+        .selectAll("path, line") // Select axis lines and ticks
+        .attr("stroke", "white"); // Set stroke to white
+
+    // Update axis text labels to white
+    intro_scatterplot_svg.selectAll(".intro_scatter_x-axis text, .intro_scatter_y-axis text")
+        .attr("fill", "white");
+    
+}
+
+
+
 
 // Initialize Scrollama
 const scroller = scrollama();
@@ -550,6 +741,9 @@ function handleStepChange(stepIndex) {
   } else if (stepIndex === 2) {
     // Focus on Action genre
     state.filters.checked = ["Action"];
+  } else if (stepIndex === 3) {
+    // Focus on Comedy genre
+    state.filters.checked = ["Romance"];
   }
 
   // Redraw the visualization with the updated filters
